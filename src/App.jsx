6ab5d4629c005c3ae7196c7f4f4156d11e4fdfb9
AnhTuanVip <tuandqhe176834@fpt.edu.vn>
2024-02-14 10:34:09 +0700

    themme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import Button from "@mui/material/Button";
-
+import Typography from "@mui/material/Typography";
 function App() {
   return (
     <>
+      <Typography variant="body2" color="text.secondary">
+        Test Tepography
+      </Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
